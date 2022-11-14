de746bade7f476474b2bdd4e66456ee745586761
Hossein <hosseinyaghoubi919@gmail.com>
2022-11-14 21:27:17 +0330

    create custome user model and set this in admin and forms

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'username', )
+    fieldsets = UserAdmin.fieldsets
+
