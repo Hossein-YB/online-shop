370cbbf43fdd635172f34dea0a9702e611425d04
Hossein <hosseinyaghoubi919@gmail.com>
2022-11-16 20:22:03 +0330

    add test for check url home and about us

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_url(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_home_page_url_name(self):
+        res = self.client.get(reverse('home'))
+        self.assertEqual(res.status_code, 200)
+
+    def test_about_us_url(self):
+        res = self.client.get('/aboutus/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_about_us_url_name(self):
+        res = self.client.get(reverse('about_us'))
+        self.assertEqual(res.status_code, 200)
