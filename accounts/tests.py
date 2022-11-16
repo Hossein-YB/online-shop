7ce43db658202f622015baba8aed12125b61a5c2
Hossein <hosseinyaghoubi919@gmail.com>
2022-11-16 22:13:03 +0330

    add test check url sing up and login

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class TestAccounts(TestCase):
+    def test_check_url_sing_up(self):
+        res = self.client.get('/accounts/singup/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_check_url_sing_up_with_name(self):
+        res = self.client.get(reverse('singup'))
+        self.assertEqual(res.status_code, 200)
+
+    def test_check_url_login(self):
+        res = self.client.get('/accounts/login/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_check_url_login_with_name(self):
+        res = self.client.get(reverse('login'))
+        self.assertEqual(res.status_code, 200)
