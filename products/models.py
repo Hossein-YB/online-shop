b60d407c51ddd5c0490accb3871042550417e1b6
Hossein <hosseinyaghoubi919@gmail.com>
2022-11-25 13:12:14 +0330

    create product detail view

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.shortcuts import reverse
 
 class Products(models.Model):
     title = models.CharField(max_length=100)
@@ -9,3 +9,6 @@ class Products(models.Model):
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id, ])
