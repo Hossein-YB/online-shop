d30081db1262f6fb59d68bc450aea56c66bfa324
Hossein <hosseinyaghoubi919@gmail.com>
2022-12-05 09:56:16 +0330

    make cart forms

---
+++ cart/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class AddToCart(forms.Form):
+    QUANTITY_CHOICES = [(i, str(i)) for i in range(30)]
+
+    quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
