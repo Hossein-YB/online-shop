80dfb373fb5ce8367eb1e30bc4ceb4c909dbd29a
Hossein <hosseinyaghoubi919@gmail.com>
2022-12-12 22:37:22 +0330

    make upload product image

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,7 +24,8 @@ urlpatterns = [
     path('',  include('pages.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('media/', include('cart.urls')),
 
 
     path('rosetta/', include('rosetta.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
