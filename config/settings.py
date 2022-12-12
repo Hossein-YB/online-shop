8d80a08d1ce88fa84f9dd80b07d897d3448db97e
Hossein <hosseinyaghoubi919@gmail.com>
2022-12-12 22:37:22 +0330

    make upload product image

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,8 @@ LOCALE_PATHS = (
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
