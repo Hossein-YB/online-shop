0515b9409f45678908b9b90e967448c5798368ec
Hossein <hosseinyaghoubi919@gmail.com>
2022-12-21 18:09:27 +0330

    create form order and use that in page

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,22 @@
+import requests
 from django.shortcuts import render
+from .forms import OrderForms
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def order_detail_view(request):
-    return render(request, 'orders/order_detail.html')
 
+    if request.method == "POST":
+
+        order_form = OrderForms(request.POST)
+
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+    else:
+        order_form = OrderForms()
+
+    return render(request, 'orders/order_detail.html', context={'order_form': order_form})
