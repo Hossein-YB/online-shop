33d2a7a39b3dffc33a3568510270066dd1a13d56
Hossein <hosseinyaghoubi919@gmail.com>
2023-01-09 21:51:17 +0330

    add blog to admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class Blog(admin.ModelAdmin):
+    list_display = ['title', 'user', 'datetime_modified', ]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
