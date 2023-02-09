8b86809af07f7128cb855c1dd0f924512910209a
Hossein <hosseinyaghoubi919@gmail.com>
2023-02-09 10:41:48 +0330

    fex bug models and admin panel blog

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import Blog, BlogCategories
+from .models import Post, BlogCategories, PostCategories, CategorySearchHistory
 
 
-@admin.register(Blog)
-class Blog(admin.ModelAdmin):
+class PostCategoryAdmin(admin.TabularInline):
+    model = PostCategories
+    extra = 2
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'user', 'datetime_modified', ]
+
+    inlines = (
+        PostCategoryAdmin,
+    )
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
@@ -12,10 +21,15 @@ class Blog(admin.ModelAdmin):
 
 
 @admin.register(BlogCategories)
-class Blog(admin.ModelAdmin):
-    list_display = ['name', 'user', 'datetime_modified', ]
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'count_search', 'user', 'datetime_modified', ]
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         super().save_model(request, obj, form, change)
+
+
+@admin.register(CategorySearchHistory)
+class CategorySearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'category']
 
--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.utils.translation import gettext as _
 from ckeditor.fields import RichTextField
 from django.shortcuts import reverse
@@ -9,7 +9,7 @@ class BlogCategories(models.Model):
     name = models.CharField(max_length=1000, verbose_name=_("category"))
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
                              related_name="categories", verbose_name=_("creator"), )
-
+    count_search = models.PositiveIntegerField(default=1)
     datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_("created datetime"))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_("update datetime"))
 
@@ -20,9 +20,25 @@ class BlogCategories(models.Model):
         return reverse('blog:blog_post_list', args=[self.id, ])
 
 
-class Blog(models.Model):
-    categories = models.ForeignKey(BlogCategories, on_delete=models.CASCADE,
-                                   related_name='category', verbose_name=(_('category')))
+class CategorySearchHistory(models.Model):
+    category = models.ForeignKey(BlogCategories, on_delete=models.CASCADE,
+                                 related_name="count_history_search", verbose_name=_("category"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                             related_name="user_search_category", verbose_name=_("user"))
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_("created datetime"))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_("update datetime"))
+
+    @classmethod
+    def insert_category_search(cls, category_id, user):
+        with transaction.atomic():
+            category = BlogCategories.objects.get(BlogCategories.id == category_id)
+            instance = cls.objects.create(category=category, user=user)
+            category.count_search += 1
+            category.save()
+            return instance
+
+
+class Post(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
                              related_name="user_posts", verbose_name=_("creator"), )
     title = models.CharField(max_length=1000, verbose_name=_("post title"))
@@ -37,5 +53,12 @@ class Blog(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.id, ])
+
+
+class PostCategories(models.Model):
+    category = models.ForeignKey(BlogCategories, on_delete=models.CASCADE,
+                                 related_name="post_categories", verbose_name=_("category"))
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="post_categories", verbose_name=_("post"))
 
 
